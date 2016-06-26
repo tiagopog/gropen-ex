039a7ffde982776b7cc6857df3e1085dccc1da87
Tiago Guedes <tiagopog@gmail.com>
2016-06-26 19:48:01 -0300

    Create CLI + the executable file

--- lib/gropen.ex
+++ lib/gropen.ex
@@ -1,10 +1,18 @@
 defmodule Gropen do
-  def for(path) do
-    repo <> "/blob/" <> branch <> "/" <> sanitize(path)
+  def main([]) do
+    print_error
   end
 
-  def sanitize(path) do
-    Regex.replace(~r/:(\d+)$/, path, "#L\\1")
+  def main(args) do
+    if correct_path?(args) do
+      hd(args) |> link_for |> IO.puts
+    else
+      print_error
+    end
+  end
+
+  def link_for(path) do
+    repo <> "/blob/" <> branch <> "/" <> sanitize(path)
   end
 
   def repo do
@@ -14,13 +22,28 @@ defmodule Gropen do
     |> hd
   end
 
-  def extract_repo_url(srt) do
-    Regex.run(~r/https?:\/\/github\.com\/\w+\/\w+/i, srt)
-  end
-
   def branch do
     System.cmd("git",  ["rev-parse", "--abbrev-ref", "HEAD"])
     |> elem(0)
     |> String.strip
+  end
+
+  defp correct_path?(args) when length(args) == 0, do: false
+
+  defp correct_path?(args) do
+    path = hd(args)
+    path && Regex.match?(~r/.+\.\w+(:\d+)?$/, path)
+  end
+
+  defp sanitize(path) do
+    Regex.replace(~r/:(\d+)$/, path, "#L\\1")
+  end
+
+  defp extract_repo_url(srt) do
+    Regex.run(~r/https?:\/\/github\.com\/\w+\/\w+/i, srt)
+  end
+
+  defp print_error do
+    IO.puts "Usage: gropen PATH [OPTIONS]"
   end
 end
--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule Gropen.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     escript: [main_module: Gropen],
      deps: deps]
   end
 
