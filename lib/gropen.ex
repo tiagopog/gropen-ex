84c9e3eff5eb630b48512f1cb3e0a80d98099bcf
Tiago Guedes <tiagopog@gmail.com>
2016-06-26 05:01:16 -0300

    Find repo's remote URL

--- lib/gropen.ex
+++ lib/gropen.ex
@@ -1,2 +1,24 @@
 defmodule Gropen do
+  def link_for(path) do
+    repo <> branch <> sanitize(path)
+  end
+
+  def sanitize(path) do
+    path
+  end
+
+  def repo do
+    System.cmd("git", ["remote", "-v"])
+    |> elem(0)
+    |> extract_repo_url
+    |> hd
+  end
+
+  def extract_repo_url(srt) do
+    Regex.run(~r/https?:\/\/github\.com\/\w+\/\w+/i, srt)
+  end
+
+  def branch do
+    "/master/"
+  end
 end
