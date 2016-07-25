217684ecb6fdfe2dc6395a94184e71b6cce78eb4
Tiago Guedes <tiagopog@gmail.com>
2016-07-24 23:37:16 -0300

    Create Git module

--- lib/gropen.ex
+++ lib/gropen.ex
@@ -15,8 +15,8 @@ defmodule Gropen do
   end
 
   def link_for(file, options \\ []) do
-    if git? do
-      remote_repo <> "/blob/"
+    if Git.present? do
+      Git.remote_repo <> "/blob/"
       |> add_branch(options)
       |> add_file(file)
       |> open
@@ -27,36 +27,21 @@ defmodule Gropen do
 
   def open(url) do
     if open? do
-      IO.puts("Remote URL: #{url}")
-      IO.puts("Opening...")
+      IO.puts("Open: #{url}")
       System.cmd("open", [url])
     else
       IO.puts(url)
     end
   end
 
-  def remote_repo do
-    System.cmd("git", ["remote", "-v"])
-    |> elem(0)
-    |> remote_repo_url
-    |> hd
-  end
-
-  def add_branch(url, options \\ []) do
+  defp add_branch(url, options) do
     branch =
-      if options[:branch] && remote_branch?(options[:branch]) do
+      if Git.remote_branch?(options[:branch]) do
         options[:branch]
       else
-        System.cmd("git",  ["rev-parse", "--abbrev-ref", "HEAD"])
-        |> elem(0)
-        |> String.strip
+        Git.current_branch
       end
     url <> branch <> "/"
-  end
-
-  defp git? do
-    {result, _} = System.cmd("git",  ["rev-parse", "--is-inside-work-tree"])
-    String.strip(result) == "true"
   end
 
   defp open? do
@@ -64,19 +49,8 @@ defmodule Gropen do
     String.length(result) > 0
   end
 
-  defp remote_branch?(branch) do
-    {result, _} = System.cmd("git", ["ls-remote", "--heads"])
-    result
-    |> String.split("\n")
-    |> Enum.any?(fn(remote) -> remote =~ ~r/heads\/#{branch}$/ end)
-  end
-
   defp add_file(url, file) do
     url <> Regex.replace(~r/:(\d+)$/, file, "#L\\1")
-  end
-
-  defp remote_repo_url(srt) do
-    Regex.run(~r/https?:\/\/github\.com\/\w+\/\w+/i, srt)
   end
 
   defp print_error(type) do
---
+++ lib/gropen/git.ex
@@ -0,0 +1,35 @@
+defmodule Git do
+  def present? do
+    {result, _} = System.cmd("git",  ["rev-parse", "--is-inside-work-tree"])
+    String.strip(result) == "true"
+  end
+
+  def remote_repo do
+    System.cmd("git", ["remote", "-v"])
+    |> elem(0)
+    |> remote_repo_url
+    |> hd
+  end
+
+  def remote_branch?(branch) do
+    if branch do
+      case System.cmd("git", ["ls-remote", "--heads"]) do
+        {result, _} ->
+          result
+          |> String.split("\n")
+          |> Enum.any?(fn(remote) -> remote =~ ~r/heads\/#{branch}$/ end)
+        _ -> false
+      end
+    end
+  end
+
+  def current_branch do
+    System.cmd("git",  ["rev-parse", "--abbrev-ref", "HEAD"])
+    |> elem(0)
+    |> String.strip
+  end
+
+  defp remote_repo_url(srt) do
+    Regex.run(~r/https?:\/\/github\.com\/\w+\/\w+/i, srt)
+  end
+end
